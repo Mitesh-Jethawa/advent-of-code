17f89081654ccf7aaaad8b69af8dae4c29b7d3b9
King1020-2 <jethawamitesh@gmail.com>
2025-12-03 17:22:22 +0530

    Day 02 Solutions

---
+++ 2025/Day-02/day_02.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+current_dir = Path(__file__).parent
+with open(current_dir/"day_02.txt") as f:
+    number_list = f.read().split(",")
+number_range_int: list[tuple[int, int]] = [(int(a), int(b)) for a, b in (x.split("-") for x in number_list)]
+password1, password2 = 0, 0
+invalid_ids = [int(str(number)*2) for number in range(1,100000)]
+complete_invalid_ids = list(set([int(str(number)*i) for i in range (2,12) for number in range(1,10**(11//i))]))
+for number_range in number_range_int:
+    start,end = number_range
+    for invalid_id in complete_invalid_ids:
+        if start <= invalid_id <= end:
+            password2 += invalid_id
+            if (invalid_id in invalid_ids):
+                password1 += invalid_id 
+print(password1, password2)
+# I'm intentionally not using regex , I wanted to try to do it without it, anyways it's very simple with regex
