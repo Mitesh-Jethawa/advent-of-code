58dfac201fe57db0a9fece84803bb4f759bfcb44
King1020-2 <jethawamitesh@gmail.com>
2025-12-04 15:35:22 +0530

    Day 3 solution

---
+++ 2025/Day-03/day_03.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+def highest_joltage(number: str, k: int):
+    start = 0
+    result = ""
+    for p in range(k):
+        end = len(number) - (k - p - 1)
+        highest_digit = max(number[start:end])
+        result += highest_digit
+        start = number.find(highest_digit, start, end) + 1
+    return int(result)
+current_dir = Path(__file__).parent
+with open(current_dir/"day_03.txt") as f:
+    numbers = f.read().splitlines()
+    password1 = sum([highest_joltage(str(number), 2) for number in numbers])
+    password2 = sum([highest_joltage(str(number), 12) for number in numbers])
+    print(password1, password2)
