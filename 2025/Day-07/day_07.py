51b8e51f3f1a47e4e986d4fd118d30f076431a7f
King1020-2 <jethawamitesh@gmail.com>
2025-12-12 03:52:49 +0530

    added a recursion solution

--- 2025/Day-07/day_07.py
+++ 2025/Day-07/day_07.py
@@ -1,6 +1,19 @@
 from pathlib import Path
+from functools import cache
+
+@cache
+def count_timelines(grid: tuple[tuple[int]], x:int, y:int) -> int:
+    if y == len(grid) - 1:
+        return 1
+    elif grid[y][x] == 1:
+        return count_timelines(grid, x-1, y+1) + count_timelines(grid, x+1, y+1)
+    elif grid[y][x] == 0 or grid[y][x] == 2:
+        return count_timelines(grid, x, y+1)
+    else:
+        return 0
+    
 SYMBOL_VALUES = {".":0, "^":1, "S":2}
-grid = [[SYMBOL_VALUES[i] for i in j] for j in Path(Path(__file__).parent/"day_07.txt").read_text().splitlines()]
+grid = tuple(tuple(SYMBOL_VALUES[i] for i in j) for j in Path(Path(__file__).parent/"day_07.txt").read_text().splitlines())
 initial_laser = grid[0].index(2)
 lasers_dict = dict.fromkeys(range(len(grid[0])), 0)
 lasers_dict[initial_laser] = 1
@@ -19,3 +32,4 @@ for row in grid:
     laser_hits = set()
 timelines = sum(lasers_dict.values())
 print(hits, timelines)
+print(count_timelines(grid, initial_laser, 0))
